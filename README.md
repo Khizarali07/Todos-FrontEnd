f63c12767f97defe705cf257a14aea885141c85f
Khizarali07 <khizarali.cs@gmail.com>
2024-10-18 06:33:46 -0700

    added new

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ In the project directory, you can run:
 ### `npm start`
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Open [https://todos-backend-production-6961.up.railway.app](https://todos-backend-production-6961.up.railway.app) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
--- src/component/main.js
+++ src/component/main.js
@@ -1,89 +1,89 @@
 import axios from "axios";
-import './css/main.css';
+import "./css/main.css";
 import del from "./images/delete icon.png";
 
 export default function main() {
   let data;
-  let task=[];
-  let i=0;
+  let task = [];
+  let i = 0;
 
-  (async function fun (){
-  data = await axios.get('http://localhost:3000/tasks');
-  task = data.data;
+  (async function fun() {
+    data = await axios.get(
+      "https://todos-backend-production-6961.up.railway.app/tasks"
+    );
+    task = data.data;
 
-  let x = document.getElementById('ma');
-  x.innerHTML="";
-  let button = document.createElement('button');
-  x.appendChild(button);
-  button.innerHTML="⟳";
-  button.onclick=refresh;
-  button.id="refresh";
-  let p = document.createElement('p');
-  x.appendChild(p);
-  p.innerHTML=`total task are :${task.length}`;
-  p.id="count";
+    let x = document.getElementById("ma");
+    x.innerHTML = "";
+    let button = document.createElement("button");
+    x.appendChild(button);
+    button.innerHTML = "⟳";
+    button.onclick = refresh;
+    button.id = "refresh";
+    let p = document.createElement("p");
+    x.appendChild(p);
+    p.innerHTML = `total task are :${task.length}`;
+    p.id = "count";
 
+    while (i < task.length) {
+      let c1 = document.createElement("div");
+      let c2 = document.createElement("p");
+      let c3 = document.createElement("p");
+      let c4 = document.createElement("button");
+      let c5 = document.createElement("button");
+      let c6 = document.createElement("img");
 
-  while (i<task.length){
-  let c1 = document.createElement('div');
-  let c2 = document.createElement('p');
-  let c3 = document.createElement('p');
-  let c4 = document.createElement('button');
-  let c5 = document.createElement('button');
-  let c6 = document.createElement('img');
-  
-  x.appendChild(c1);
-  c1.appendChild(c4); 
-  c1.appendChild(c2);
-  c1.appendChild(c3); 
-  c1.appendChild(c5);
-  c5.appendChild(c6);
-  
-  c1.className="listing";
-  c2.className="ts";
-  c3.className="da";
-  c4.className="combut";
-  c4.onclick=complete;
-  c5.className="delbut";
-  c6.className="delimg";
-  c5.onclick=deletee;
-  c2.innerHTML=task[i].task;
-  c3.innerHTML=task[i].date;
-  if (window.screen.width <= 599) {
-    c6.src=del;
-  } else {
-    c5.innerHTML="Delete";
-  }
-  
-  
-  if (task[i].complete === "complete"){
-  c4.innerHTML="&#10004;";
-  }
-  i++;
-  }
-})();
-console.log(del);
+      x.appendChild(c1);
+      c1.appendChild(c4);
+      c1.appendChild(c2);
+      c1.appendChild(c3);
+      c1.appendChild(c5);
+      c5.appendChild(c6);
 
-  return (
-    <div id="ma" >
-      
-    </div>
-  )
+      c1.className = "listing";
+      c2.className = "ts";
+      c3.className = "da";
+      c4.className = "combut";
+      c4.onclick = complete;
+      c5.className = "delbut";
+      c6.className = "delimg";
+      c5.onclick = deletee;
+      c2.innerHTML = task[i].task;
+      c3.innerHTML = task[i].date;
+      if (window.screen.width <= 599) {
+        c6.src = del;
+      } else {
+        c5.innerHTML = "Delete";
+      }
+
+      if (task[i].complete === "complete") {
+        c4.innerHTML = "&#10004;";
+      }
+      i++;
+    }
+  })();
+  console.log(del);
+
+  return <div id="ma"></div>;
 }
 
-
-function complete (evt) {
-  let complete="complete";
-  let task=evt.target.nextSibling.innerHTML;
+function complete(evt) {
+  let complete = "complete";
+  let task = evt.target.nextSibling.innerHTML;
   console.log(task);
-  axios.post('http://localhost:3000/complete',{complete,task})
+  axios.post("https://todos-backend-production-6961.up.railway.app/complete", {
+    complete,
+    task,
+  });
   window.location.reload();
 }
-function deletee (evtt) {
-  let date=evtt.target.previousSibling.innerHTML;
-  axios.post('http://localhost:3000/delete',{date});
+function deletee(evtt) {
+  let date = evtt.target.previousSibling.innerHTML;
+  axios.post("https://todos-backend-production-6961.up.railway.app/delete", {
+    date,
+  });
   window.location.reload();
 }
-function refresh (){
+function refresh() {
   window.location.reload();
 }
